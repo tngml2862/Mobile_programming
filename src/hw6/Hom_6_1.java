721cd36b945d4f27d190a182d4a6db354bab6bda
tngml2862 <tngml2862@naver.com>
2015-10-15 23:27:18 +0900

    6주차1번째 과제제출

---
+++ src/hw6/Hom_6_1.java
@@ -0,0 +1,36 @@
+package hw6;
+
+import java.io.*;
+
+class AAA
+{
+	public void readFile()
+	{
+		try {
+			FileWriter reader=new FileWriter("c:\\a.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void close() {
+	
+	}
+}
+public class Hom_6_1 {
+	public static void main(String[] args) 
+	{
+		
+		AAA a=new AAA();
+		a.readFile();
+		
+		String path = AAA.class.getResource("").getPath(); 
+		File path1 = new File(".");  System.out.println(path1.getAbsolutePath());
+		
+		a.close();
+	}
+}
+
+
+
